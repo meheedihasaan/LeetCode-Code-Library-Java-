a3751fd17402cd421b73f54e78d574d0ca1108ac
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-11-09 22:59:39 +0600

    Add files via upload

--- BFS_And_DFS/NumberOfClosedIslands.java
+++ BFS_And_DFS/NumberOfClosedIslands.java
@@ -8,6 +8,43 @@ package BFS_And_DFS;
  *
  * @author User
  */
+
+//Problem Link: https://leetcode.com/problems/number-of-closed-islands/
+
 public class NumberOfClosedIslands {
+    //Time Complexity: O(n*m), Space Complexity: O(n*m)
+    public int closedIsland(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 0){
+                    if(dfs(i, j, grid)){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
     
+    public boolean dfs(int i, int j, int grid[][]){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length){
+            return false;
+        }
+        
+        if(grid[i][j] == 1){
+            return true;
+        }
+        
+        grid[i][j] = 1;
+        
+        boolean f1 = dfs(i, j+1, grid);
+        boolean f2 = dfs(i, j-1, grid);
+        boolean f3 = dfs(i+1, j, grid);
+        boolean f4 = dfs(i-1, j, grid);
+        
+        return f1 && f2 && f3 && f4;
+    }
 }
