1d2022058621c27ebe45e22378bc5abb17a6ccec
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-11-15 00:42:33 -0800

    Update UnionFindExample.java

--- DisjointSet/UnionFindExample.java
+++ DisjointSet/UnionFindExample.java
@@ -9,28 +9,34 @@ package DisjointSet;
  * @author User
  */
 
+//UnionFind Class
 class UnionFind{
     public int parent[];
     public int rank[];
+    public int size[];
+    
     public UnionFind(int n){
         parent = new int[n+1];
         rank = new int[n+1];
+        size = new int[n+1];
         for(int i = 1; i <= n; i++){
             parent[i] = i;
-            rank[i] = 0;
+            size[i] = 1;
         }
-    }
+    }    
     
+    //Finding root-parent of a Node
     public int findParent(int node){
         if(node == parent[node]){
             return node;
         }
-        
+        //Path Compression
         parent[node] = findParent(parent[node]);
         return parent[node];
     }
     
-    public void union(int u, int v){
+    //Doing Union operation by Rank
+    public void unionByRank(int u, int v){
         u = findParent(u);
         v = findParent(v);
         
@@ -51,18 +57,50 @@ class UnionFind{
             rank[u]++;
         }
     }
+    
+    //Doing Union operation by Size    
+    public void unionBySize(int u, int v){
+        u = findParent(u);
+        v = findParent(v);
+        
+        //Check if both node's parent are same
+        if(u == v){
+            System.out.println("Both nodes are in same component");
+            return;
+        }
+        
+        if(size[u] < size[v]){
+            parent[u] = v;
+            size[v]+=size[u];  //Update size of the component
+        }
+        else{
+            parent[v] = u;
+            size[u]+=size[v];  //Update size of the component 
+        }
+    }
+    
+    //Counting number of components
+    int cnt = 0;
+    public int countComponents(){        
+        for (int i = 0; i < parent.length; i++) {
+            if(parent[i] == i){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
 
 public class UnionFindExample {
     public static void main(String[] args) {
         UnionFind un = new UnionFind(20);
-        un.union(1, 2);
-        un.union(2,3);
-        un.union(1, 3);   //1 and 3 both are in same component
-        un.union(4, 5);
-        un.union(6, 7);
-        un.union(4, 6);
-        un.union(5, 7);   //5 and 7 both are in same component
+        un.unionByRank(1, 2);
+        un.unionByRank(2,3);
+        un.unionByRank(1, 3);   //1 and 3 both are in same component
+        un.unionByRank(4, 5);
+        un.unionByRank(6, 7);
+        un.unionByRank(4, 6);
+        un.unionByRank(5, 7);   //5 and 7 both are in same component
 //        System.out.println(un.findParent(2));
 //        System.out.println(un.findParent(3));
 //        System.out.println(un.findParent(4));
