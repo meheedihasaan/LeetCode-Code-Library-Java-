b8722079c8857680f0ed59eb7654a6a33a97e37e
Mehedi Hasan <105338310+meheedihasaan@users.noreply.github.com>
2022-11-25 23:13:58 +0600

    Add files via upload

---
+++ Heap/MaximizeSumOfArrayAfterKNegations.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Heap;
+
+import java.util.PriorityQueue;
+
+/**
+ *
+ * @author User
+ */
+
+//Problem Link: https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
+public class MaximizeSumOfArrayAfterKNegations {
+    public int largestSumAfterNegations(int[] nums, int k){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i = 0; i < nums.length; i++){
+            minHeap.offer(nums[i]);
+        }
+        
+        while(k > 0){
+            int x = minHeap.poll();
+            x*=(-1);
+            minHeap.offer(x);
+            k--;
+        }
+        
+        int ans = 0;
+        while(!minHeap.isEmpty()){
+            ans+=minHeap.poll();
+        }
+        return ans;
+    }
+}
